b42842aa59676ed17dfec2a88ad4e48762eb96ca
Yury <92sawik@gmail.com>
2023-10-11 13:50:08 +0300

    Пример начала интерфейса с переходом к таблицам БД

---
+++ interface.py
@@ -0,0 +1,75 @@
+import sys
+import sqlite3
+from PyQt5 import QtWidgets
+
+class DatabaseManager:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def get_data(self, table_name):
+        self.cursor.execute(f"SELECT * FROM {table_name}")
+        data = self.cursor.fetchall()
+        return data
+
+    def __del__(self):
+        self.conn.close()
+
+class TableDialog(QtWidgets.QDialog):
+    def __init__(self, table_name, data):
+        super().__init__()
+        self.setWindowTitle(table_name)
+        layout = QtWidgets.QVBoxLayout()
+        table_widget = QtWidgets.QTableWidget()
+        table_widget.setRowCount(len(data))
+        table_widget.setColumnCount(len(data[0]))
+
+        # Установка размеров ячеек таблицы
+        header = table_widget.horizontalHeader()
+        header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+        table_widget.verticalHeader().setDefaultSectionSize(50)
+
+        for i, row in enumerate(data):
+            for j, item in enumerate(row):
+                table_widget.setItem(i, j, QtWidgets.QTableWidgetItem(str(item)))
+
+        layout.addWidget(table_widget)
+        self.setLayout(layout)
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        layout = QtWidgets.QVBoxLayout()
+        categories_button = QtWidgets.QPushButton('Категории')
+        warehouses_button = QtWidgets.QPushButton('Склады')
+        clients_button = QtWidgets.QPushButton('Клиенты')
+        orders_button = QtWidgets.QPushButton('Заказы')
+
+        layout.addWidget(categories_button)
+        layout.addWidget(warehouses_button)
+        layout.addWidget(clients_button)
+        layout.addWidget(orders_button)
+
+        categories_button.clicked.connect(lambda: self.show_table_dialog('ProductCategories'))
+        warehouses_button.clicked.connect(lambda: self.show_table_dialog('Warehouses'))
+        clients_button.clicked.connect(lambda: self.show_table_dialog('Customers'))
+        orders_button.clicked.connect(lambda: self.show_table_dialog('Orders'))
+
+        self.setLayout(layout)
+
+    def show_table_dialog(self, table_name):
+        data = self.db_manager.get_data(table_name)
+        dialog = TableDialog(table_name, data)
+        dialog.exec_()
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.db_manager = DatabaseManager('inventory_management.db')  # Имя базы данных
+    main_window.show()
+    sys.exit(app.exec_())
