94be96606bd4b0c3aca6a6c7ccd3345999adb0b6
Yury <92sawik@gmail.com>
2023-10-13 19:16:30 +0300

    заменил кнопку на продукты + привязка к БД

--- interface.py
+++ interface.py
@@ -39,7 +39,41 @@ class TableDialog(QtWidgets.QDialog):
                 table_widget.setItem(i, j, QtWidgets.QTableWidgetItem(str(item)))
 
         layout.addWidget(table_widget)
+
+        # Добавление кнопок "Удалить", "Добавить", "Изменить", "Поиск товара"
+        delete_button = QtWidgets.QPushButton('Удалить')
+        add_button = QtWidgets.QPushButton('Добавить')
+        edit_button = QtWidgets.QPushButton('Изменить')
+        search_button = QtWidgets.QPushButton('Поиск товара')
+
+        layout.addWidget(delete_button)
+        layout.addWidget(add_button)
+        layout.addWidget(edit_button)
+        layout.addWidget(search_button)
+
+        # Подключение кнопок к функциям
+        delete_button.clicked.connect(self.delete_item)
+        add_button.clicked.connect(self.add_item)
+        edit_button.clicked.connect(self.edit_item)
+        search_button.clicked.connect(self.search_item)
+
         self.setLayout(layout)
+
+    def delete_item(self):
+        # Реализация удаления записи из базы данных
+        pass
+
+    def add_item(self):
+        # Реализация добавления новой записи в базу данных
+        pass
+
+    def edit_item(self):
+        # Реализация редактирования записи в базе данных
+        pass
+
+    def search_item(self):
+        # Реализация поиска товара
+        pass
 
 
 class MainWindow(QtWidgets.QWidget):
@@ -50,7 +84,7 @@ class MainWindow(QtWidgets.QWidget):
     def initUI(self):
         self.setGeometry(100, 100, 200, 300)  # Установка размеров окна
         layout = QtWidgets.QVBoxLayout()
-        categories_button = QtWidgets.QPushButton('Категории')
+        products_button = QtWidgets.QPushButton('Продукты')
         warehouses_button = QtWidgets.QPushButton('Склады')
         clients_button = QtWidgets.QPushButton('Клиенты')
         orders_button = QtWidgets.QPushButton('Заказы')
@@ -58,17 +92,17 @@ class MainWindow(QtWidgets.QWidget):
         # Установка размеров кнопок
         button_width = 200
         button_height = 60
-        categories_button.setFixedSize(button_width, button_height)
+        products_button.setFixedSize(button_width, button_height)
         warehouses_button.setFixedSize(button_width, button_height)
         clients_button.setFixedSize(button_width, button_height)
         orders_button.setFixedSize(button_width, button_height)
 
-        layout.addWidget(categories_button)
+        layout.addWidget(products_button)
         layout.addWidget(warehouses_button)
         layout.addWidget(clients_button)
         layout.addWidget(orders_button)
 
-        categories_button.clicked.connect(lambda: self.show_table_dialog('ProductCategories'))
+        products_button.clicked.connect(lambda: self.show_table_dialog('Products'))
         warehouses_button.clicked.connect(lambda: self.show_table_dialog('Warehouses'))
         clients_button.clicked.connect(lambda: self.show_table_dialog('Customers'))
         orders_button.clicked.connect(lambda: self.show_table_dialog('Orders'))
@@ -84,6 +118,6 @@ class MainWindow(QtWidgets.QWidget):
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     main_window = MainWindow()
-    main_window.db_manager = DatabaseManager('inventory_management.db')  # Укажите имя вашей базы данных
+    main_window.db_manager = DatabaseManager('inventory_management.db')
     main_window.show()
     sys.exit(app.exec_())
